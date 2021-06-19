7b3cf8417a8e12eee1a6159f97411681299cceb2
jin hanada <jin_hanada@protonmail.com>
2021-06-20 02:22:09 +0900

    wip put name

--- tools/forth.sol
+++ tools/forth.sol
@@ -1,1 +1,11 @@
+: forth
+  : dict
+    : here+! ( n -- ) here + align here! ; # aligned
+    : create ( name -- )
+      : put_name ( str -- &name ) here >r dup s:len here+! i s:copy r> ;
+      put_name # -- &name
+    ;
+  ;
+;
+
 : main "forth" prn ;
