05912fd0f503eef5a03ead510fb1c48a68cf2ea1
jin hanada <jin_hanada@protonmail.com>
2021-06-22 17:52:51 +0900

    add test

--- tools/forth_test.sol
+++ tools/forth_test.sol
@@ -3,9 +3,15 @@ include: "forth.sol"
 
 : test_dict
   "create" [ "abcd" forth:dict:create yes ] CHECK
-  "create align" [ forth:dict:latest dup align = ] CHECK
-  "create name"  [ forth:dict:latest forth:dict:name "abcd" s= ] CHECK
-  "create xt"    [ forth:dict:latest forth:dict:xt here = ] CHECK
+
+  "create/align" [ forth:dict:latest dup align = ] CHECK
+  "create/name"  [ forth:dict:latest forth:dict:name "abcd" s= ] CHECK
+  "create/xt"    [ forth:dict:latest forth:dict:xt here = ] CHECK
+
+  "create/handler" [
+    forth:dict:latest forth:dict:handler
+    &forth:dict:handle_normal =
+  ] CHECK
 ;
 
 : main
