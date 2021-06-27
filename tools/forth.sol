7e2a063d7b64d3d44cd6e2b8e40396679ee07887
jin hanada <jin_hanada@protonmail.com>
2021-06-27 12:49:41 +0900

    reorder

--- tools/forth.sol
+++ tools/forth.sol
@@ -79,17 +79,6 @@
     dict:latest loop
   ;
   
-  ( ----- eval ----- )
-  : eval_token_from ( name link -- found? )
-    dict:find_from not IF no RET END # -- header
-    [ dict:xt ] [ dict:handler ] biq mode swap call ok ;
-  ( testing from latest )
-  : eval_token ( name -- found? ) dict:latest eval_token_from ;
-  : eval_token_in ( name mode -- found? )
-    mode >r mode! eval_token r> mode! ;
-  : run_token     ( name -- found? ) run_mode     eval_token_in ;
-  : compile_token ( name -- found? ) compile_mode eval_token_in ;
-
   ( ----- setup primitives ----- )
   : primitives
     : primcode ( n -- code ) 1 << 1 bit-or ;
@@ -153,14 +142,16 @@
     ;
   ;
 
-  ( handler 2 )
-  : handle_data ( xt state -- )
-    compile_mode [ "LIT" compile_token drop , ] ;CASE
-    run_mode     [ ( push xt )                ] ;CASE
-    unknown_mode
-  ;
-
-  ( eval )
+  ( ----- eval ----- )
+  : eval_token_from ( name link -- found? )
+    dict:find_from not IF no RET END # -- header
+    [ dict:xt ] [ dict:handler ] biq mode swap call ok ;
+  ( testing from latest )
+  : eval_token ( name -- found? ) dict:latest eval_token_from ;
+  : eval_token_in ( name mode -- found? )
+    mode >r mode! eval_token r> mode! ;
+  : run_token     ( name -- found? ) run_mode     eval_token_in ;
+  : compile_token ( name -- found? ) compile_mode eval_token_in ;
   : eval ( str -- ... )
     val: instack
     val: insp
@@ -211,6 +202,13 @@
       parse_num      IF eval_num ok RET END
       buf epr " ?" eprn ng
     ] while
+  ;
+
+  ( ----- handler 2 ----- )
+  : handle_data ( xt state -- )
+    compile_mode [ "LIT" compile_token drop , ] ;CASE
+    run_mode     [ ( push xt )                ] ;CASE
+    unknown_mode
   ;
 
   ( setup core words )
