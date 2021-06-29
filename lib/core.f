950ff61caa8c320a4995021872a8b0fbc8d101db
jin hanada <jin_hanada@protonmail.com>
2021-06-29 12:49:05 +0900

    add struct

--- lib/core.f
+++ lib/core.f
@@ -22,17 +22,45 @@
 
 ( ----- Module ----- )
 
-: close-module ( start end -- )
-  [ 2dup = [ 2drop STOP ] ;IF
-    dup forth:hide! forth:next GO
-  ] while
-;
-
 : MODULE ( -- start start closer )
-  forth:latest dup &close-module ;
+  forth:latest dup
+  [ ( start end -- )
+    [ 2dup = [ 2drop STOP ] ;IF
+      dup forth:hide! forth:next GO
+    ] while
+  ]
+;
 
 : ---EXPOSE--- ( start start closer -- start latest closer )
   nip forth:latest swap ;
+
+
+
+( ----- string ----- )
+
+
+: s:append! ( dst what -- )
+  ( no check )
+  >r [ dup b@ IF inc GO ELSE STOP THEN ] while r> ( dst what )
+  [ 2dup b@ swap over swap b!
+    IF &inc bia GO ELSE 2drop STOP THEN
+  ] while
+;
+
+: s:append ( dst what max -- ? )
+  # max includes null termination
+  ( check )
+  >r 2dup &s:len bia + inc r>
+  > [ 2drop ng ] ;IF
+  s:append! ok
+;
+
+
+: s:check ( str max -- str ok? )
+  # check length
+  # max includes null termination
+  over s:len 1 + >=
+;
 
 
 
@@ -43,36 +71,33 @@ MODULE
   32 const: len
   len allot const: buf
   len 1 - const: max
-
+  
   : check ( name -- name )
     dup s:len max > IF epr " too long" panic THEN
   ;
-
+  
   : create_getter ( addr -- )
     buf forth:create
     "LIT" forth:compile, , "@" forth:compile, "RET" forth:compile,
   ;
-
+  
   : create_setter ( addr -- )
-    buf [
-      dup b@
-      0 [ dup 33 ( ! ) swap b! inc 0 swap b! STOP ] ;CASE
-      drop inc GO
-    ] while
+    buf "!" max s:append drop
     buf forth:create
     "LIT" forth:compile, , "!" forth:compile, "RET" forth:compile,
   ;
-
+  
 ---EXPOSE---
-
+  
   : val: ( v -- )
-    in:read not IF "val name required" panic THEN check
+    in:read not IF "val name required" panic THEN
+    max s:check [ epr " ...too long" panic ] unless
     buf s:copy
     here:align! here swap , dup
     create_getter
     create_setter
   ;
-
+  
 END
 
 
@@ -89,3 +114,30 @@ END
   ] while
   cr
 ;
+
+
+
+( ----- struct ----- )
+
+MODULE
+
+  : close ( -- word offset ) swap forth:xt! ;
+
+---EXPOSE---
+
+  : STRUCT ( -- word offset q )
+    in:read [ "struct name required" panic ] unless
+    forth:create
+    &handle:data forth:latest forth:handler!
+    forth:latest 0 &close
+  ;
+  
+  : field: ( offset q n -- offset+n q)
+    in:read [ "field name required" panic ] unless
+    forth:create
+    swap >r over
+    "LIT" forth:compile, , "+" forth:compile, "RET" forth:compile,
+    + r>
+  ;
+  
+END
--- lib/sarkam.f
+++ lib/sarkam.f
@@ -18,45 +18,45 @@ require: lib/app.f
 
 MODULE
 
-# bresenham's algorithm
-
-0 val: x0  0 val: y0
-0 val: x1  0 val: y1
-0 val: dx  0 val: dy
-0 val: sx  0 val: sy
-0 val: e1  0 val: e2
-
-: CHECK
-  x0 x1 != IF RET THEN
-  y0 y1 != IF RET THEN
-  rdrop ( exit loop )
-;
-
-: loop
-  x0 y0 plot
-  CHECK
-  e1 2 * e2!
-  e2 dy neg > IF
-    e1 dy - e1!
-    x0 sx + x0!
-  THEN
-  e2 dx < IF
-    e1 dx + e1!
-    y0 sy + y0!
-  THEN
-  AGAIN
-;
-
+  # bresenham's algorithm
+  
+  0 val: x0  0 val: y0
+  0 val: x1  0 val: y1
+  0 val: dx  0 val: dy
+  0 val: sx  0 val: sy
+  0 val: e1  0 val: e2
+  
+  : CHECK
+    x0 x1 != IF RET THEN
+    y0 y1 != IF RET THEN
+    rdrop ( exit loop )
+  ;
+  
+  : loop
+    x0 y0 plot
+    CHECK
+    e1 2 * e2!
+    e2 dy neg > IF
+      e1 dy - e1!
+      x0 sx + x0!
+    THEN
+    e2 dx < IF
+      e1 dx + e1!
+      y0 sy + y0!
+    THEN
+    AGAIN
+  ;
+  
 ---EXPOSE---
 
-: line ( x0 y0 x1 y1 -- )
-  y1! x1! y0! x0!
-  x1 x0 - abs dx!
-  y1 y0 - abs dy!
-  x1 x0 > IF 1 ELSE -1 THEN sx!
-  y1 y0 > IF 1 ELSE -1 THEN sy!
-  dx dy - e1!
-  loop
-;
-
+  : line ( x0 y0 x1 y1 -- )
+    y1! x1! y0! x0!
+    x1 x0 - abs dx!
+    y1 y0 - abs dy!
+    x1 x0 > IF 1 ELSE -1 THEN sx!
+    y1 y0 > IF 1 ELSE -1 THEN sy!
+    dx dy - e1!
+    loop
+  ;
+  
 END
