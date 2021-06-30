600c190b448efc114979619af29c6233c223b63c
jin hanada <jin_hanada@protonmail.com>
2021-07-01 00:53:18 +0900

    add mgui mouse

--- test/sarkam.f
+++ test/sarkam.f
@@ -1,13 +1,10 @@
 require: lib/sarkam.f
 require: lib/basic_sprite.f
 require: lib/entity.f
+require: lib/mgui.f
 
 basic.spr:load
 
-
-val: mx
-val: my
-&mx &my mouse:pos!
 
 val: x 10 x!
 val: y 10 y!
@@ -15,7 +12,7 @@ val: w 20 w!
 val: h 20 h!
 
 : drawrect
-  mx my x y w h hover_rect?
+  mouse:x mouse:y x y w h hover_rect?
   [ x y w h fill_rect ] [ x y w h rect ] if
 ;
 
