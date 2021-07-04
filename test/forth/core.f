7104429f35814c5819c1919a9a852d73c1db7a9a
jin hanada <jin_hanada@protonmail.com>
2021-07-04 21:03:09 +0900

    add core.f test

---
+++ test/forth/core.f
@@ -0,0 +1,43 @@
+require: lib/core.f
+
+
+
+MARKER: <TEST-TOOLS>
+
+ok "ASSERT" ASSERT
+
+"CHECK" [ ok ] CHECK
+
+<TEST-TOOLS>
+
+
+
+# ( ng "COMMENT" ASSERT
+( ng "COMMENT" ASSERT )
+
+
+
+MARKER: <STACK>
+
+"tuck" [ 1 2 tuck 2drop 2 = ] CHECK
+
+"?dup 0" [ 0 ?dup 0 = ] CHECK
+"?dup 1" [ 1 ?dup + 2 = ] CHECK
+
+"pullup" [
+  1 2 3 pullup
+  1 = "pullup TOS" ASSERT
+  3 = "pullup 2nd" ASSERT
+  2 = "pullup 3rd" ASSERT
+  ok
+] CHECK
+
+"pushdown" [
+  1 2 3 pushdown
+  2 = "pushdown TOS" ASSERT
+  1 = "pushdown 2nd" ASSERT
+  3 = "pushdown 3rd" ASSERT
+  ok
+] CHECK
+
+<STACK>
