014f99c2902eda332ca9be05850c088fe29409ca
jin hanada <jin_hanada@protonmail.com>
2021-07-15 15:08:33 +0900

    add combinator tests

--- test/forth/core.f
+++ test/forth/core.f
@@ -235,6 +235,41 @@ len dec as: max
 
 
 
+MARKER: <COMBINATOR>
+
+" dip 1" [ 1 2 [ inc ] dip + ( => 2 2 + ) 4 = ] CHECK
+
+
+" sip 1" [ 1 [ inc ] sip + ( => 2 1 + ) 3 = ] CHECK
+
+
+" biq 1" [ 1 [ 1 + ] [ 2 + ] biq + ( => 2 3 +     ) 5 = ] CHECK
+" biq 2" [ 1 [ 1   ] [ + + ] biq   ( => 1 1 1 + + ) 3 = ] CHECK
+
+
+" bia 1" [ 1 2 [ inc ] bia + ( => 2 3 + )     5 = ] CHECK
+" bia 2" [ 1 2 3 [ + ] bia   ( => 1 2 + 3 + ) 6 = ] CHECK
+
+
+" bi* 1" [ 1 2 [ 1 + ] [ 2 + ] bi* + ( => 2 4 + )     6 = ] CHECK
+" bi* 2" [ 2 3 [ 1   ] [ + + ] bi*   ( => 2 1 3 + + ) 6 = ] CHECK
+
+
+" bibi 1" [ 1 2 [ + ] [ 1 + + ] bibi + ( => 3 4 + ) 7 = ] CHECK
+
+
+" triq 1" [ 1 [ 1 + ] [ 2 + ] [ 3 + ] triq + + ( => 2 3 4 + + ) 9 = ] CHECK
+
+
+" tria 1" [ 1 2 3 [ inc ] tria + + ( => 2 3 4 + + ) 9 = ] CHECK
+
+
+" tri* 1" [ 1 2 3 [ inc ] [ inc ] [ inc ] tri* + + ( => 2 3 4 + + ) 9 = ] CHECK
+
+<COMBINATOR>
+
+
+
 MARKER: <VAR>
 
 var: x
