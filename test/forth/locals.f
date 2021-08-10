96bbc1db7e2edf5ca59313eeabe88c3d02f77683
jin hanada <jin_hanada@protonmail.com>
2021-08-10 19:51:16 +0900

    add test for locals

--- test/forth/locals.f
+++ test/forth/locals.f
@@ -2,8 +2,17 @@ require: lib/locals.f
 
 MARKER: <LOCALS>
 
-: foo { $a $b | $c -- a+b } $a $b + $c! $c ;
+: foo { a b | c -- a+b } a b + c! c ;
 
 1 2 foo 3 = ASSERT" locals"
+
+( check overwrite locals )
+
+: a 100 ;
+: b 200 ;
+: c 300 ;
+
+: foo { a b | c -- a+b } a b + c! c ;
+1 2 foo 3 = ASSERT" overwrite"
 
 <LOCALS>
