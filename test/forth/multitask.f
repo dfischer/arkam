57b952d2865a2f4ec0b37f0f3dc7ac4375c47be5
jin hanada <jin_hanada@protonmail.com>
2021-08-22 08:55:39 +0900

    change convention

--- test/forth/multitask.f
+++ test/forth/multitask.f
@@ -2,30 +2,30 @@ require: lib/multitask.f
 
 LEXI [multi] REFER
 
-mes: inc
-mes: sum
+mes: $inc
+mes: $sum
 
 COVER
     var: count
 SHOW
     [ [
-        inc [ parcel count + count! ] ;case
-        sum [ count sum sender SEND ] ;case
+        $inc [ parcel count + count! ] ;case
+        $sum [ count $sum sender SEND ] ;case
     ] recv ] task: counter
 END
 
 
-[ [ drop 1 inc counter SEND ] recv ] task: inc1
+[ [ drop 1 $inc counter SEND ] recv ] task: inc1
 
-[ [ drop 1 inc counter SEND ] recv ] task: inc2
+[ [ drop 1 $inc counter SEND ] recv ] task: inc2
 
 
 3 [
-    0 1 inc1 SEND PAUSE
-    0 1 inc2 SEND PAUSE
+    0 $inc inc1 SEND PAUSE
+    0 $inc inc2 SEND PAUSE
 ] times
 
-0 sum counter SEND
-RECV sum = ASSERT" receive sum from counter"
+0 $sum counter SEND
+RECV $sum = ASSERT" receive sum from counter"
 parcel ( 3*2 ) 6 = ASSERT" parcel is sum"
 
